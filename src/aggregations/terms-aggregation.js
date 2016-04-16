18600a5ba10bb19d805c2eff4a8281471214ee97
Nicolás Fantone <nfantone@gmail.com>
2016-04-15 23:51:03 -0300

    Allow options to be set on terms agg

--- src/aggregations/terms-aggregation.js
+++ src/aggregations/terms-aggregation.js
@@ -1,17 +1,18 @@
+import _ from 'lodash'
+
 /**
  * Construct a Terms aggregation.
  *
  * @param  {String} field Field name to aggregate over.
  * @param  {String} name  Aggregation name. Defaults to agg_terms_<field>.
+ * @param  {Object} opts  Additional options to include in the aggregation.
  * @return {Object}       Terms aggregation.
  */
-export default function termsAggregation(field, name) {
+export default function termsAggregation(field, name, opts) {
   name = name || `agg_terms_${field}`
   return {
     [name]: {
-      terms: {
-        field: field
-      }
+      terms: (() => _.assign({field}, opts))()
     }
   }
 }

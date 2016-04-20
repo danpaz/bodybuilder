f2439aaa7d24585fad3751c3b75c258c8e85ab2b
Nicolás Fantone <nfantone@contentamobile.com>
2016-04-20 12:24:14 -0300

    Make use of ES6 default param values on topHitsAggregation

--- src/aggregations/top-hits-aggregation.js
+++ src/aggregations/top-hits-aggregation.js
@@ -7,15 +7,15 @@ let count = 0;
 /**
  * Construct a Top hits aggregation.
  *
- * @param  {Object} opts  Options to include in the aggregation (maybe empty).
- * @param  {String} name  Aggregation name. Defaults to agg_top_hits_{count}.
+ * @param  {Object} opts  Options to include in the aggregation. Defaults to {}.
+ * @param  {String} name  Aggregation name. Defaults to 'agg_top_hits_{count}'.
  * @return {Object}       Top hits Aggregation.
  */
-export default function topHitsAggregation(opts, name) {
+export default function topHitsAggregation(opts = {}, name) {
   name = name || `agg_top_hits_${count++}`
   return {
     [name]: {
-      top_hits: opts || {}
+      top_hits: opts
     }
   }
 }

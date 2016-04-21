3f3881703df552874491a5098c54bfc98786cd04
npatmaja <noval.78@gmail.com>
2016-04-21 15:02:35 +0700

    Add type to multi match query
    Why:
    To add capability to specify type for multi match
    query
    
    This change addresses the need by:
    Adding a new parameter called `type` defaulted to
    `best_fields` (Elasticsearch default)

--- src/queries/multi-match-query.js
+++ src/queries/multi-match-query.js
@@ -3,12 +3,14 @@
  *
  * @param  {Array}  fields  The field names to query over.
  * @param  {String} query   The query string.
+ * @param  {String} type    The type of multi_match query (optional).
  * @return {Object}         Multi Match query.
  */
-export default function multiMatchQuery(fields, query) {
+export default function multiMatchQuery(fields, query, type='best_fields') {
   return {
     multi_match: {
       query: query,
+      type: type,
       fields: fields
     }
   }

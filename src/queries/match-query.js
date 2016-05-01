c9b33e6454f4441ef0a17847a9b58dfb973f4973
Ronak Patel <rspatel@gmail.com>
2016-05-01 17:42:55 -0500

    Add phrase option to match query

--- src/queries/match-query.js
+++ src/queries/match-query.js
@@ -1,14 +1,26 @@
 /**
  * Construct a Match query.
  *
- * @param  {String} field Field name to query over.
- * @param  {String} term  Query value.
- * @return {Object}       Match query.
+ * @param  {String}  field    Field name to query over.
+ * @param  {String}  term     Query value.
+ * @param  {Boolean} isPhrase Should the term be treated as a phrase or not.
+ * @return {Object}           Match query.
  */
-export default function matchQuery(field, term) {
-  return {
-    match: {
-      [field]: term
+export default function matchQuery(field, term, isPhrase = false) {
+  if (isPhrase) {
+    return {
+      match: {
+        [field]: {
+          query: term,
+          type: 'phrase'
+        }
+      }
+    }
+  } else {
+    return {
+      match: {
+        [field]: term
+      }
     }
   }
 }

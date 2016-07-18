c64ba19b898f528a436a58b388f0bce1e6a39693
Gaurav Arora <garora@nimbuzz.com>
2016-07-18 16:47:05 +0530

    add range aggregation

---
+++ src/aggregations/range-aggregation.js
@@ -0,0 +1,18 @@
+import _ from 'lodash'
+
+/**
+ * Construct a Range aggregation.
+ *
+ * @param  {String} field Field name to aggregate over.
+ * @param  {String} name  Aggregation name. Defaults to agg_terms_<field>.
+ * @param  {Object} opts  Additional options to include in the aggregation.
+ * @return {Object}       Range aggregation.
+ */
+export default function rangeAggregation(field, name, opts) {
+  name = name || `agg_range_${field}`
+  return {
+    [name]: {
+      range: (() => _.assign({field}, opts))()
+    }
+  }
+}

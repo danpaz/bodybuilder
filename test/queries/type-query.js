843ea8da0b20f5f32b3003251cb6298b038e06dd
Gaurav Arora <garora@nimbuzz.com>
2016-10-15 18:45:46 +0530

    fix import name

--- test/queries/type-query.js
+++ test/queries/type-query.js
@@ -1,10 +1,10 @@
-import termQuery from '../../src/queries/type-query'
+import typeQuery from '../../src/queries/type-query'
 import {expect} from 'chai'
 
 describe('typeQuery', () => {
 
   it('should create a type query', () => {
-    let result = termQuery('custom-type')
+    let result = typeQuery('custom-type')
     expect(result).to.eql({
       type: {
         value: 'custom-type'

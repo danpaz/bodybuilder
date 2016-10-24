512681028a4b0b38f95fe43bfb52684545e3afc3
Mirco Sanguineti <mirco@sanguineti.gr>
2016-10-24 17:44:58 +0300

    "npm run check" caught something :)

--- src/queries/nested-query.js
+++ src/queries/nested-query.js
@@ -4,7 +4,6 @@ import {assign} from 'lodash'
  * Construct a Nested query.
  * https://www.elastic.co/guide/en/elasticsearch/guide/current/nested-query.html
  *
- * @private
  * @memberof Queries
  *
  * @param  {String} path      The nested object the query is referring to. See https://www.elastic.co/guide/en/elasticsearch/guide/current/nested-query.html#CO272-2
@@ -12,11 +11,11 @@ import {assign} from 'lodash'
  * @param  {Object} opts      Right now, the only optional field is score_mode. See https://www.elastic.co/guide/en/elasticsearch/guide/current/nested-query.html#CO273-1 
  * @return {Object}           Nested query.
  */
-export default function nestedQuery(path, nested_query, opts = {}) {
+export default function nestedQuery(path, query, opts = {}) {
   return {
     nested: assign({
       path: path,
-      query: nested_query
+      query: query
     }, opts)
   }
 }

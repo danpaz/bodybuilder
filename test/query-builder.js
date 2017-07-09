8a0d7c0f3b3dd211dcd9a5603f1493f0feedf671
Johannes Scharlach <johannes@shareiq.com>
2017-07-09 14:35:44 +0200

    add test for hasParent

--- test/query-builder.js
+++ test/query-builder.js
@@ -471,6 +471,23 @@ test('queryBuilder | has_parent', (t) => {
   })
 })
 
+test('queryBuilder | hasParent (valid v2 syntax)', (t) => {
+  t.plan(1)
+
+  const result = queryBuilder().query('hasParent', 'parentTag', 'blog', (q) => {
+    return q.query('term', 'tag', 'something')
+  })
+
+  t.deepEqual(result.getQuery(), {
+    hasParent: {
+      parentTag: 'blog',
+      query: {
+        term: { tag: 'something' }
+      }
+    }
+  })
+})
+
 test('queryBuilder | geo_bounding_box', (t) => {
   t.plan(1)
 
